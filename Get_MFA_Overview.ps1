8cda3b668868fc02f1efb2c8138bb1d7ab3c92a2
fdhin <fd@envo-it.dk>
2023-01-08 16:09:07 +0100

    first commit

---
+++ Get_MFA_Overview.ps1
@@ -0,0 +1,52 @@
+# Connect to Microsoft Online Services (Azure Active Directory)
+Connect-MsolService
+
+# Initialize an empty array to store results
+$results = @()
+
+# Get a list of all users in the directory
+$users = Get-MsolUser -All
+
+# Loop through each user in the list
+foreach ($user in $users) {
+    # Get strong authentication requirements, phone app details, and methods
+    $mfaStatus = $user.StrongAuthenticationRequirements.State
+    $phoneApp = $user.StrongAuthenticationPhoneAppDetails
+    $methodTypes = $user.StrongAuthenticationMethods
+
+    # If MFA is enabled or there are authentication methods, set variables based on user details
+    if ($mfaStatus -ne $null -or $methodTypes -ne $null) {
+        if ($mfaStatus -eq $null) {
+            $mfaStatus = "Enabled (Conditional Access)"
+        }
+        $authMethods = $methodTypes.MethodType
+        $defaultAuthMethod = ($methodTypes | Where {$_.IsDefault -eq "True"}).MethodType
+        $verifyEmail = $user.StrongAuthenticationUserDetails.Email
+        $phoneNumber = $user.StrongAuthenticationUserDetails.PhoneNumber
+        $alternativePhoneNumber = $user.StrongAuthenticationUserDetails.AlternativePhoneNumber
+    }
+    # If MFA is disabled, set variables to null
+    else {
+        $mfaStatus = "Disabled"
+        $defaultAuthMethod = $null
+        $verifyEmail = $null
+        $phoneNumber = $null
+        $alternativePhoneNumber = $null
+    }
+
+    # Add a new object to the results array with user details
+    $results += New-Object PSObject -Property @{
+        UserName = $user.DisplayName
+        UserPrincipalName = $user.UserPrincipalName
+        MFAStatus = $mfaStatus
+        AuthenticationMethods = $authMethods
+        DefaultAuthMethod = $defaultAuthMethod
+        MFAEmail = $verifyEmail
+        PhoneNumber = $phoneNumber
+        AlternativePhoneNumber = $alternativePhoneNumber
+        DeviceName = $phoneApp.DeviceName
+    }
+}
+
+# Select certain properties from the results array and export them to a CSV file
+$results | Select UserName,UserPrincipalName,MFAStatus,DefaultAuthMethod,MFAEmail,PhoneNumber,AlternativePhoneNumber,DeviceName | Export-Csv MFAReport.csv
